8060875152216a8c7e44edaaea767b359b25b36e
Muhammad Bilal <mbilalraza1106@gmail.com>
2024-04-25 18:05:27 +0500

    Add ATM machine

---
+++ app.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+let myBalance = 10000; // Dollar
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "userPin",
+        type: "number",
+        message: "enter your pin"
+    }
+]);
+if (pinAnswer.userPin == myPin) {
+    console.log("Correct Pin Code");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "please select option",
+            choices: ["withdraw", "check balance"]
+        }
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "number",
+                message: "enter your amount"
+            }
+        ]);
+        //  =, -=, +=
+        myBalance -= amountAns.amount;
+        console.log("your remaining balance is:", +myBalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your remaining balance is:", +myBalance);
+    }
+    else {
+        console.log("Wrong Pin Code");
+    }
+}
